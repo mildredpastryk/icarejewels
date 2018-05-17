be91b0bf3105ee4818313e4c6bfccb407236d0bb
mildredpastryk <mildred.pastryk@gmail.com>
2018-05-17 08:46:03 +0200

    3.8 add root to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'simple_pages/index'
+
+  root 'simple_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
