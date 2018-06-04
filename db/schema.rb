a6c89bc655efe9cf4492db8bfb3b101aeb3f8dc3
mildredpastryk <mildred.pastryk@gmail.com>
2018-06-04 12:40:55 +0200

    4.2 generate db migration - product color

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_04_061745) do
+ActiveRecord::Schema.define(version: 2018_06_04_102532) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_06_04_061745) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "color"
   end
 
 end
