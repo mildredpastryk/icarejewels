05b42b499dfaecf9a632b717c9567ad45e90b08d
mildredpastryk <mildred.pastryk@gmail.com>
2018-06-08 18:57:16 +0200

    4.6 orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,11 @@
 class OrdersController < ApplicationController
 
   def index
-    @orders = Order.all
+    @orders = Order.includes(:product).all
   end
 
   def show
+    @order = Order.find(params[:id])
   end
 
   def new
