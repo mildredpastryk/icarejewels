862ec1eed3c8ce7243d725c37582601ae4e00740
mildredpastryk <mildred.pastryk@gmail.com>
2018-06-11 18:42:58 +0200

    5.1 change root path, add product page to nav, redirect create product to about

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  resources :products
+  resources :products, :orders
   get 'simple_pages/about'
   get 'simple_pages/contact'
-  get 'simple_pages/index'
+  root 'simple_pages#index'
 
-  root 'simple_pages#landing_page'
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
