3b99d7d7a3e379e43a821a5bc642abe5d567caa9
mildredpastryk <mildred.pastryk@gmail.com>
2018-06-19 11:16:50 +0200

    5.5 feat: User's authorization & add alert.js

---
+++ app/assets/javascripts/alerts.js
@@ -0,0 +1,5 @@
+$(document).on ('turbolinks:load ajaxSuccess', function() {
+  $(".alert").delay(2000).fadeOut(1000)
+});
+
+
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
-	rescue_from CanCan::AccessDenied do |exception|
-  		redirect_to main_app.root_url, alert: exception.message
-	end
+	#rescue_from CanCan::AccessDenied do |exception|
+  		#redirect_to main_app.root_url, alert: exception.message
+	#end
 
 end
