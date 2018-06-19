8ea24426007752e6c2635183446ae47013d8e733
mildredpastryk <mildred.pastryk@gmail.com>
2018-06-19 19:15:55 +0200

    5.7 feat: Comment model & database

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-    has_many :orders
+  has_many :orders
+  has_many :comments
+
 end
