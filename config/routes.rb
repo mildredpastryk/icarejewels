e6182a2ffd1fade455c40e4063b44049a0678388
mildredpastryk <mildred.pastryk@gmail.com>
2018-06-20 11:27:27 +0200

    5.8 Add product review form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :products do
+    resources :comments
+  end
+
   resources :users
-  resources :products
+
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
