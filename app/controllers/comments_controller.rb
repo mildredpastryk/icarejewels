bbe1c5812f9a0d38e5e4d6de08195932a73e9bdb
mildredpastryk <mildred.pastryk@gmail.com>
2018-06-27 15:52:28 +0200

    Fix comment order & pagination / Add Devise params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -25,7 +25,7 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-   	params.require(:comment).permit(:user_id, :body, :rating)
+   	params.require(:comment).permit(:body, :rating)
   end
 
 end
